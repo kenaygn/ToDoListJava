a5d0b5e28c38197456120c861f501f2a2c0a60c8
kenay <kenay@localhost>
2025-08-26 10:57:37 -0300

    Constructors with requested overload

--- ToDoListJava/src/main/java/ucb/aplicacao/model/Tarefas.java
+++ ToDoListJava/src/main/java/ucb/aplicacao/model/Tarefas.java
@@ -4,13 +4,46 @@
  */
 package ucb.aplicacao.model;
 
+import java.time.LocalDateTime;
 /**
  *
  * @author kenay
  */
 public class Tarefas {
+    
+    private Long id;
     private String titulo;
     private String descricao;
-    private String horario;
-    private boolean concluido;
+    private LocalDateTime horario;
+    private boolean completa;
+    
+    //Construtor padrao
+    
+    public Tarefas(){
+        this.horario = LocalDateTime.now();
+        this.completa = false;
+    }
+    
+    //Construtor com titulo
+    
+    public Tarefas(String titulo){
+        this();
+        this.titulo = titulo;
+    }
+    
+    //Construtor com titulo e descricao
+    
+    public Tarefas(String titulo, String descricao){
+        this(titulo);
+        this.descricao = descricao;
+    
+    }
+    
+    //Construtor Completo
+    
+    public Tarefas(Long id, String titulo, String descricao, boolean completa){
+        this(titulo, descricao);
+        this.id = id;
+        this.completa = completa;
+    }
 }
