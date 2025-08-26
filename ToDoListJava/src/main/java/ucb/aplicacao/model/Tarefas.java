15815bb348a4e225ac3e975ca68e61cbad7157e4
kenay <kenay@localhost>
2025-08-26 10:38:17 -0300

    Adding atributes

---
+++ ToDoListJava/src/main/java/ucb/aplicacao/model/Tarefas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucb.aplicacao.model;
+
+/**
+ *
+ * @author kenay
+ */
+public class Tarefas {
+    private String titulo;
+    private String descricao;
+    private String horario;
+    private boolean concluido;
+}
---
+++ ToDoListJava/src/main/java/ucb/aplicacao/view/ToDoListJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucb.aplicacao.view;
+
+/**
+ *
+ * @author kenay
+ */
+public class ToDoListJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
