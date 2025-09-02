481b82ee5e0ce913858e5c11d67ccd8edf3dcb61
kenay <kenay@localhost>
2025-09-02 10:10:31 -0300

    getters and setters

--- ToDoListJava/src/main/java/ucb/aplicacao/model/Tarefas.java
+++ ToDoListJava/src/main/java/ucb/aplicacao/model/Tarefas.java
@@ -14,7 +14,7 @@ public class Tarefas {
     private Long id;
     private String titulo;
     private String descricao;
-    private LocalDateTime horario;
+    private LocalDateTime dataHora;
     private boolean completa;
     
     //Construtor padrao
@@ -44,6 +44,76 @@ public class Tarefas {
     public Tarefas(Long id, String titulo, String descricao, boolean completa){
         this(titulo, descricao);
         this.id = id;
+        this.completa = completa;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the dataHora
+     */
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    /**
+     * @param dataHora the dataHora to set
+     */
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    /**
+     * @return the completa
+     */
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    /**
+     * @param completa the completa to set
+     */
+    public void setCompleta(boolean completa) {
         this.completa = completa;
     }
 }
